f9a5a82751271f90d8a03797272dafbd7f6f7808
IsaacLira42 <isaaclira422@gmail.com>
2026-06-19 20:34:07 -0300

    feat: adiciona componente hero com ícones do FontAwesome e atualiza dependências

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-
+import { faGithub, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-export class Hero {}
+export class Hero {
+  faGithub = faGithub;
+  faDownload = faDownload;
+  faWhatsapp = faWhatsapp;
+  faInstagram = faInstagram;
+}
