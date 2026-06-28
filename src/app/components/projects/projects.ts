a68f4a0485503a580af7f8ca0cc69c42e5d44ac0
IsaacLira42 <isaaclira422@gmail.com>
2026-06-28 20:50:43 -0300

    feat: ajusta a descrição e adiciona mais imagens nos projetos

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -31,14 +31,14 @@ export class Projects {
     {
       title: 'LiraBank',
       description:
-        'Simulação de banco digital desenvolvida para demonstrar o domínio de uma stack moderna...',
+        'Simulação de banco digital full stack com Node.js/TypeScript e React, focada em arquitetura em camadas, segurança (JWT, bcrypt), validação com Zod e integridade transacional usando PostgreSQL + Prisma.',
       image: 'assets/images/projetos/lirabank-1.png',
       tags: ['React', 'Express'],
     },
     {
       title: 'EcommerceNextJs',
       description:
-        'Plataforma e-commerce com Next.js e PostgreSQL. Funcionalidades: listagem de produtos...',
+        'Aplicação de e-commerce desenvolvida com Next.js, React.js e PostgreSQL + Prisma, implementando persistência de carrinho e fluxo completo de compra.',
       image: 'assets/images/projetos/lirashop-1.png',
       images: [
         'assets/images/projetos/lirashop-1.png',
@@ -51,20 +51,27 @@ export class Projects {
       title: 'Mini Twitter',
       description:
         'Aplicação inspirada no Twitter com foco em arquitetura de frontend, gerenciamento de estado e integração com API REST.',
-      image: 'assets/images/projetos/capa-mini_twitter.png',
-      tags: ['TypeScript', 'Angular'],
+      image: 'assets/images/projetos/mini_twitter-1.png',
+      images: [
+        'assets/images/projetos/mini_twitter-1.png',
+        'assets/images/projetos/mini_twitter-2.png',
+        'assets/images/projetos/mini_twitter-3.png',
+      ],
+      tags: ['TypeScript', 'React', 'Zustand', 'Axios'],
     },
     {
       title: 'N8N - Sistema Multiagente',
-      description:
-        'Sistema de gerenciamento de agentes autônomos, desenvolvido com Python e Django.',
+      description: 'Sistema de gerenciamento de agentes autônomos, desenvolvido com N8N e lovable',
       image: 'assets/images/projetos/capa-sistema_multiagente.png',
-      tags: ['Python', 'Django'],
+      images: ['assets/images/projetos/capa-sistema_multiagente.png'],
+      tags: ['N8N', 'lovable'],
     },
     {
       title: 'Bomberman 1984',
-      description: 'Jogo de arcade clássico recriado com Assembly Mips.',
+      description:
+        'Jogo de arcade clássico inspirado no clássico jogo de 1984, recriado com Assembly Mips.',
       image: 'assets/images/projetos/capa-bomberman.png',
+      images: ['assets/images/projetos/capa-bomberman.png'],
       tags: ['Assembly', 'Mips Mars'],
     },
     {
@@ -72,6 +79,7 @@ export class Projects {
       description:
         'aplicativo construído em Flutter que consome a API do SUAP (via um backend em Django) para atuar como uma camada de suporte à decisão. O foco do projeto é converter o volume de dados acadêmicos brutos em informações acionáveis, auxiliando o aluno no monitoramento de desempenho e na gestão de riscos de reprovação.',
       image: 'assets/images/projetos/capa-suapin.png',
+      images: ['assets/images/projetos/capa-suapin.png'],
       tags: ['Flutter'],
     },
   ]);
